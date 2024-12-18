5f81d60deab2b74f666c257876dd57c3ea386016
Witek G <witold.grybel@gmail.com>
2024-12-18 18:20:45 +0100

    Some changes.

--- src/main.rs
+++ src/main.rs
@@ -10,31 +10,35 @@ use std::{env, time::SystemTime};
 use chrono::prelude::*;
 use moon_phase;
 
+const WEEK_NAMES : [&str; 7] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let local: DateTime<Local> = Local::now();
     let greet = greeting_text(local.hour());
-    // dbg!(args);
     if args.len() < 2 {
         println!("{}, whoever you are!", greet);
     } else {
         println!("{}, {}!", greet, args[1]);
     }
-    println!("Today is {}.{}.{}, and time is {}:{}:{}", 
-        local.day0(), local.month(), local.year(), local.hour(), local.minute(), local.second());
+
+    let weekday = local.weekday();
+    let custom_date = local.format("%Y.%m.%d");
+    let custom_time = local.format("%H:%M:%S");
+    println!("Today is {}, {}, and time is {}",
+        WEEK_NAMES[weekday.num_days_from_monday() as usize], custom_date, custom_time);
+    
     let mp = moon_phase::MoonPhase::new(SystemTime::now());
     println!("The Moon is {}, and it is in {}", mp.phase_name, mp.zodiac_name);
 }
 
 fn greeting_text(hour: u32) -> String {
-    let mut str = String::from("Good ");
     if hour < 12 {
-        str = str + "morning";
+        return "Good morning".to_string();
     } else if hour < 17 {
-        str = str + "afternoon"
+        return "Good afternoon".to_string();
     } else {
-        str = str + "evening";
+        return "Good evening".to_string();
     }
-    return str;
 }
 
